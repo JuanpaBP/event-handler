2a945934f54e5b5acb99ac6072871bbf5f083676
necronomicus <hernanpizarrohepp@gmail.com>
2024-10-05 12:16:12 -0300

    agregando findById y findAll

--- src/main/java/persistence/EventDAO.java
+++ src/main/java/persistence/EventDAO.java
@@ -2,9 +2,12 @@ package persistence;
 
 import configuration.HibernateConfig;
 import entity.Event;
+import jakarta.ws.rs.ProcessingException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class EventDAO {
     EntityManagerFactory emf = HibernateConfig.setup();
@@ -15,6 +18,33 @@ public class EventDAO {
         em.persist(event);
         em.getTransaction().commit();
         em.close();
+    }
+
+    public Event getEventById(Integer id) {
+        try {
+            String query = "Select e from Event e where e.id = :id";
+            TypedQuery<Event> typedQuery = em.createQuery(query, Event.class);
+            typedQuery.setParameter("id", id);
+            return typedQuery.getSingleResult(); //La query tiene la capacidad de auto ejecutarse.
+        } catch (ProcessingException e) {
+            throw e;
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public List<Event> getAllEvents() {
+        try {
+            String query = "Select e from Event e";
+            TypedQuery<Event> typedQuery = em.createQuery(query, Event.class);
+            return typedQuery.getResultList();
+        } catch (ProcessingException e) {
+            throw e;
+        } finally {
+            em.close();
+        }
+
     }
 
 }
